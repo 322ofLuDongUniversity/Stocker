47553212185ffd3f2eda8c0e3f6478ff986a0aca
jxch <15553598935@163.com>
2020-03-23 16:53:28 +0800

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\xicheng_jiang\work home\Workspace\PyCharm\stocker\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
